63a90246255cd59430eb09a401d6eae285f3d93c
Alex Kudlick <adkudlick@gmail.com>
2012-05-29 19:50:04 -0700

    add root controller, search method on events

--- app/js/events.js
+++ app/js/events.js
@@ -1,1 +1,14 @@
 EV = Events = Ember.Application.create();
+
+EV.eventsController = Ember.ArrayController.create({
+	content: [],
+	loadUpcoming: function() {
+		$.get('/api/events', function(data) {
+			EV.eventsController.set('content',data);
+		});
+	}
+});
+
+EV.listView = Ember.CollectionView.extend({
+	contentBinding: 'EV.eventsController.content'
+});
