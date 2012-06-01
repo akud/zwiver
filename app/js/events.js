7213972256940ba963950a2654cb52756d0d2902
Alex Kudlick <adkudlick@gmail.com>
2012-05-31 20:35:17 -0700

    fleshing out basics for pulling up events

--- app/js/events.js
+++ app/js/events.js
@@ -2,13 +2,24 @@ EV = Events = Ember.Application.create();
 
 EV.eventsController = Ember.ArrayController.create({
 	content: [],
+
+	/**
+	  * Load upcoming events
+		*/
 	loadUpcoming: function() {
 		$.get('/api/events', function(data) {
 			EV.eventsController.set('content',data);
 		});
-	}
+	} 
 });
 
-EV.listView = Ember.CollectionView.extend({
-	contentBinding: 'EV.eventsController.content'
+EV.listView = Ember.CollectionView.create({
+	contentBinding: Ember.Binding.oneWay('EV.eventsController.content'),
+	tagName: 'ul',
+	itemViewClass: Ember.View.extend({templateName: 'list-item'})
 });
+/*
+EV.mapView = Ember.View.create({
+	locationsBinding: 'EV.eventsController.content',
+});
+*/
