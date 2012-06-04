de37d056db6521060736dc2d2b0878bfe540b642
Alex Kudlick <adkudlick@gmail.com>
2012-06-03 18:18:22 -0700

    add in google maps

--- app/js/events.js
+++ app/js/events.js
@@ -1,14 +1,30 @@
 EV = Events = Ember.Application.create();
 
+EV.Event = Ember.Object.extend({
+	maxDescriptionLength: 140,
+	position: function() {
+		return new google.maps.LatLng(this.get('lat'),this.get('lon'));
+	}.property('lat','lon').cacheable(),
+
+	shortDescription: function() {
+		var desc = this.get('description') || '';
+		var maxLength = this.get('maxDescriptionLength');
+		return desc.length < maxLength ? desc : desc.substring(0,maxLength) + '...';
+	}.property('description').cacheable()
+});
+
 EV.eventsController = Ember.ArrayController.create({
 	content: [],
-
 	/**
 	  * Load upcoming events
 		*/
-	loadUpcoming: function() {
+	init: function() {
+		this._super();
 		$.get('/api/events', function(data) {
-			EV.eventsController.set('content',data);
+			EV.eventsController.set('content', data.map(function(item) {
+				return EV.Event.create(item);
+				})
+			);
 		});
 	} 
 });
@@ -18,8 +34,42 @@ EV.listView = Ember.CollectionView.create({
 	tagName: 'ul',
 	itemViewClass: Ember.View.extend({templateName: 'list-item'})
 });
-/*
+
 EV.mapView = Ember.View.create({
-	locationsBinding: 'EV.eventsController.content',
+	contentBinding: Ember.Binding.oneWay('EV.eventsController.content'),
+	map: null,
+	mapOpts: { 
+		center: new google.maps.LatLng(37.78,-122.442),
+		zoom: 12,
+		disableDefaultUI: true,
+		zoomControl: true,
+		panControl: true,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	},
+	//content may be loaded before the map is ready,
+	//so observe both properties
+	markers: [],
+	mapObserver: function() {
+		var markers = this.get('markers')
+		var map = this.get('map');
+		//reset markers, if any
+		markers.forEach(function(marker){ marker.setMap(null); });
+		markers.length = 0;
+		//add new markers to map
+		if(map) {
+			this.get('content').forEach(function(item) {
+				markers.push(new google.maps.Marker({
+					map: map,
+					position: item.get('position'),
+					title: item.title
+				}));
+			});
+		}
+	}.observes('map','content.@each'),
+	//initialize the map when we're appending to the dom
+	appendTo: function(selector) {
+		this._super(selector);
+		this.set('map', 
+			new google.maps.Map($(selector).get(0),	this.get('mapOpts')));
+	}
 });
-*/
