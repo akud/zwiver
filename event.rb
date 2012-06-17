1e0d4b4067d4b885ebd31b77776f503b17da333d
Alex Kudlick <adkudlick@gmail.com>
2012-06-16 17:25:34 -0700

    code formatting

--- event.rb
+++ event.rb
@@ -2,21 +2,21 @@ require 'json'
 require 'net/http'
 
 class Event
-	
-	EVENT_URL = URI.parse('http://localhost/api/events/')
+  
+  EVENT_URL = URI.parse('http://localhost/api/events/')
 
-	def initialize args
-		@body = args
-	end
+  def initialize args
+    @body = args
+  end
 
 
-	def post
-		response = Net::HTTP.post_form EVENT_URL, { 
-			:event => @body.to_json
-		}
-		unless response.is_a? Net::HTTPSuccess
-			puts "Failed to save event for #{@body[:url]}"
-			puts "#{response.code}: #{response.message}"
-		end
-	end
+  def post
+    response = Net::HTTP.post_form EVENT_URL, { 
+      :event => @body.to_json
+    }
+    unless response.is_a? Net::HTTPSuccess
+      puts "Failed to save event for #{@body[:url]}"
+      puts "#{response.code}: #{response.message}"
+    end
+  end
 end
--- warfield.rb
+++ warfield.rb
@@ -7,42 +7,42 @@ require 'event'
 
 class WarfieldScraper 
 
-	def initialize 
-		@agent = Mechanize.new { |agent|
-			agent.user_agent_alias = 'Mac Safari'
-		}
-		@venue = 'The Warfield'
-		@address = '982 Market St, San Francisco, CA 94102'
-	end
+  def initialize 
+    @agent = Mechanize.new { |agent|
+      agent.user_agent_alias = 'Mac Safari'
+    }
+    @venue = 'The Warfield'
+    @address = '982 Market St, San Francisco, CA 94102'
+  end
 
-	def scrape_all
-		@agent.get('http://thewarfieldtheatre.com/events.php') do |page|
-			page.links_with( :href => /^eventdetail/).each do |link|
-				event_page = link.click
-				title = event_page.search('h3').text + ' ' + event_page.search('p.supporting_acts').text
-		
-				info_items = event_page.search 'div#main div ul li'
-				day = info_items[0].text.sub 'Day: ', ''
-				time = info_items[1].text.sub 'Showtime: ', ''
-				date = Chronic.parse(day + ' ' + time)
-				description = ''
-				event_page.search('div.right').each do |tag|
-					if tag.name.eql? 'text'
-						description += tag.text.strip
-					end
-				end
-				Event.new(
-					:title => title,
-					:date => date,
-					:description => description,
-					:address => @address,
-					:venue => @venue
-				).post
-			end
-		end
-	end
+  def scrape_all
+    @agent.get('http://thewarfieldtheatre.com/events.php') do |page|
+      page.links_with( :href => /^eventdetail/).each do |link|
+        event_page = link.click
+        title = event_page.search('h3').text + ' ' + event_page.search('p.supporting_acts').text
+    
+        info_items = event_page.search 'div#main div ul li'
+        day = info_items[0].text.sub 'Day: ', ''
+        time = info_items[1].text.sub 'Showtime: ', ''
+        date = Chronic.parse(day + ' ' + time)
+        description = ''
+        event_page.search('div.right').each do |tag|
+          if tag.name.eql? 'text'
+            description += tag.text.strip
+          end
+        end
+        Event.new(
+          :title => title,
+          :date => date,
+          :description => description,
+          :address => @address,
+          :venue => @venue
+        ).post
+      end
+    end
+  end
 end
 
 if $0 == __FILE__
-	WarfieldScraper.new.scrape_all
+  WarfieldScraper.new.scrape_all
 end
