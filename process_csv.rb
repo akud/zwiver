24495e2c3575e732e34984a9aac52c0a002b13e4
Alex Kudlick <adkudlick@gmail.com>
2012-06-24 15:54:33 -0700

    include state in address

--- process_csv.rb
+++ process_csv.rb
@@ -3,6 +3,7 @@ CSV.foreach(ARGV[0]) do |row|
   name = row[0].split.map {|w| w.capitalize}.join ' '
   addr = row[3].split.map {|w| w.capitalize}.join ' '
   city = row[4].split.map {|w| w.capitalize}.join ' '
-  zip = row[5]
-  puts name + ',' + ([addr, city, zip].join ' ')
+  state = row[5] 
+  zip = row[6].split('-')[0]
+  puts ([name , addr, city, state, zip].join ',')
 end
