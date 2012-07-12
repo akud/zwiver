05c710e6618d8b7aff4ac6df6512ea299e8d1386
Alex Kudlick <adkudlick@gmail.com>
2012-07-11 20:14:42 -0700

    script for google calendar (no urls there, so remove the validation)

--- scrape_google_calendar.rb
+++ scrape_google_calendar.rb
@@ -1,16 +1,19 @@
-calendar_id = 'zwiver.com_gniohjo4b9130ajkrrpc34jbd0@group.calendar.google.com'
-require "rubygems"
-require "gcal4ruby"
+#!/usr/bin/ruby -I.
+require 'rubygems'
+require 'gcal4ruby'
+require 'zwiver_event'
 
+calendar_id = 'zwiver.com_gniohjo4b9130ajkrrpc34jbd0@group.calendar.google.com'
 gcal = GCal4Ruby::Service.new
-gcal.authenticate "alexk@zwiver.com", "fl1NNtpH"
+gcal.authenticate 'alexk@zwiver.com', 'fl1NNtpH'
 
 events = GCal4Ruby::Event.find gcal, :calendar => calendar_id, :futureevents => true
 
 events.find_all {|e| e.attendees.map {|a| a[:name]}.include? 'Annalysa_cal'}.each do |event|
-    puts event.title
-    puts event.content
-    puts event.start_time
-    puts '-----------------------'
+  Zwiver::Event.new(:title => event.title,
+    :description => event.content,
+    :date => event.start_time,
+    :venue => event.where.split('|')[0],
+    :address => event.where.split('|')[1]
+  ).post
 end
-
