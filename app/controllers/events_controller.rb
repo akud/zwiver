a72845e67d7459481c64881ebb88b65442ec785b
Alex Kudlick <adkudlick@gmail.com>
2012-07-12 16:03:59 -0700

    some better error states

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,29 +1,41 @@
 class EventsController < ApplicationController
  
   def index
-    params[:start] ||= 0
-    params[:limit] ||= 10
-    @events = Event.find_upcoming params[:limit], params[:start]
-    json = {}
-    json[:events] = @events
-    json[:next] = {
-      :start => params[:start].to_i + 10
-    }
-    json[:prev] = {
-      :start => params[:start].to_i - 10
-    } if params[:start].to_i > 0
+    begin
+      params[:start] ||= 0
+      params[:limit] ||= 10
+      @events = Event.find_upcoming params[:limit], params[:start]
+      json = {}
+      json[:events] = @events
+      json[:next] = {
+        :start => params[:start].to_i + 10
+      }
+      json[:prev] = {
+        :start => params[:start].to_i - 10
+      } if params[:start].to_i > 0
 
-    render :json => json
+      render :json => json, :status => :ok
+    rescue => e
+      render :status => :internal_server_error, :json => {:error => e.to_s}
+    end
   end
 
   def show 
     @event = Event.find params[:id]
-    render :json => @event
+    if @event
+      render :json => @event
+    else 
+      render :status => :not_found, :nothing => true
+    end
   end
 
   def search 
-    @events = Event.search params[:terms]
-    render :json => @events
+    begin
+      @events = Event.search params[:terms]
+      render :json => @events
+    rescue => e
+      render :status => :internal_server_error, :json => {:error => e.to_s} 
+    end
   end
 
   def create 
@@ -36,7 +48,7 @@ class EventsController < ApplicationController
         render :nothing => true, :status => :created, :location => @event
       else
         Rails.logger.error "Failed to create event: #{@event.errors.to_json}"
-        render :json => @event.errors.to_json, :status => :error
+        render :json => @event.errors.to_json, :status => :bad_request
       end
     end
   end
