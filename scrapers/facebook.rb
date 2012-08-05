3f681cebcde414de49424adb9c60c0ce885b353f
Alex Kudlick <adkudlick@gmail.com>
2012-08-05 15:19:37 -0700

    patch from scrapers repo

--- scrapers/facebook.rb
+++ scrapers/facebook.rb
@@ -2,84 +2,87 @@
 require 'restclient'
 require 'json'
 
-
-class FB
-  @@access_token = '400109626693462|D0Xr_U9eHnr6VWJR7yZECL9lQx8'
-  @@base_url = 'https://graph.facebook.com'
-  @@search_url = 'https://graph.facebook.com/search'
+module FB
+  DEFAULT_ACCESS_TOKEN = '400109626693462|D0Xr_U9eHnr6VWJR7yZECL9lQx8'
+  BASE_URL = 'https://graph.facebook.com'
+  SEARCH_URL = 'https://graph.facebook.com/search'
 
   def self.search params
-    params[:access_token] = @@access_token
+    params[:access_token] ||= DEFAULT_ACCESS_TOKEN
     params[:fields] ||= 'id'
-    JSON.parse(RestClient.get @@search_url, :params => params)
+    puts "searching: #{BASE_URL}?#{params.map{|k,v| k.to_s + '=' + v.to_s}.join '&'}"
+    JSON.parse(RestClient.get SEARCH_URL, :params => params)
   end
 
-  def self.get id
-    JSON.parse(RestClient.get "#{@@base_url}/#{id}", 
-      :params => { :access_token => @@access_token })
+  def self.get id, access_token=nil
+    url = "#{BASE_URL}/#{id}"
+    access_token ||= DEFAULT_ACCESS_TOKEN
+    puts "Loading #{url}?access_token=#{access_token}"
+    JSON.parse(RestClient.get url, 
+      :params => { :access_token => access_token})
   end
-end
 
 #Wrapper for a list of items from Facebook's graph api
-class FBList < FB
-  attr_reader :list
+  class List 
+    attr_reader :list
 
-  def initialize item_class, params
-    @item_class = item_class
-    data = FB.search params
-    make_list! data
-    make_next_params! data
-  end
-
-  def load_next!
-    if @next_params
-      response = FB.search @next_params
-      make_list! response
-      make_next_params! response
-    else
-      @list = []
+    def initialize item_class, params
+      @item_class = item_class
+      data = FB.search params
+      make_list! data, params[:access_token]
+      make_next_params! data
     end
-  end
 
-  def has_next?; !!@next_params; end
-
-  private 
-
-  def make_next_params! fb_response
-    @next_params = nil 
-    if fb_response['paging'] and fb_response['paging']['next']
-      @next_params = {}
-      fb_response['paging']['next'].split('?')[1].split('&').each do |str|
-        arr = str.split '='
-        @next_params[arr[0]] = arr[1]
+    def load_next!
+      if @next_params
+        response = FB.search @next_params
+        make_list! response, @next_params[:access_token]
+        make_next_params! response
+      else
+        @list = []
       end
     end
-  end
 
-  def make_list! fb_response
-    @list = fb_response['data'].map {|place| @item_class.new place['id'] }
-  end
+    def has_next?; !!@next_params; end
 
-end
+    private 
+
+    def make_next_params! fb_response
+      @next_params = nil 
+      if fb_response['paging'] and fb_response['paging']['next']
+        @next_params = {}
+        fb_response['paging']['next'].split('?')[1].split('&').each do |str|
+          arr = str.split '='
+          @next_params[arr[0].to_sym] = arr[1]
+        end
+      end
+    end
+
+    def make_list! fb_response, access_token
+      @list = fb_response['data'].map {|place| @item_class.new place['id'], access_token }
+    end
+
+  end
 
 
 #Hash-like wrapper around the information for a single fb item
-class FBItem < FB
-  attr_reader :data
+  class Item 
+    attr_reader :data
 
-  def initialize id
-    @data = FB.get id
-  end
+    def initialize id, access_token
+      @data = FB.get id, access_token
+    end
 
-  def [] key
-    @data[key]
-  end
+    def [] key
+      @data[key]
+    end
 
-  def []= key, value
-    @data[key] = value
-  end
+    def []= key, value
+      @data[key] = value
+    end
 
-  def to_json
-    JSON.pretty_generate @data
-  end
-end 
+    def to_json
+      JSON.pretty_generate @data
+    end
+  end 
+end
--- scrapers/scrape_fb_events.rb
+++ scrapers/scrape_fb_events.rb
@@ -5,22 +5,25 @@ require 'zwiver_event'
 require 'time'
 require 'chronic'
 
-class EventList < FBList
-  def initialize query
+class EventList < FB::List
+  def initialize query, access_token=FB::DEFAULT_ACCESS_TOKEN
     require 'chronic'
     super Event, :q => query,
       :type => 'event',
-      :until => Chronic.parse('next sunday at midnight').to_i
+      :until => Chronic.parse('next sunday at midnight').to_i,
+      :access_token => access_token
   end
 end
 
-class Event < FBItem
+class Event < FB::Item
   
   include Pointlike
   include Zwiver::Saveable
 
-  def initialize id
-    super id
+  attr_reader :url, :title, :description, :date, :venue, :address, :lat, :lon
+
+  def initialize id, access_token
+    super id, access_token
     if @data['venue']
       @lat = @data['venue']['latitude']
       @lon = @data['venue']['longitude']
@@ -39,13 +42,21 @@ class Event < FBItem
 end
 
 if $0 == __FILE__
-  ('a'..'z').each do |letter|
-    events = EventList.new letter
-    begin
-      events.list.find_all {|e| e.has_location? && e.distance_from(37.7750,-122.4183) < 20}.each do |e|
-        e.save
-      end
-      events.load_next!
-    end while events.has_next?
+  access_tokens = IO.readlines("#{File.dirname(__FILE__)}/data/fb_access_tokens.txt").map {|t| t.strip}
+  access_tokens << FB::DEFAULT_ACCESS_TOKEN
+
+  terms = IO.readlines("#{File.dirname(__FILE__)}/data/fb_search_terms.txt").map {|t| t.strip}
+  terms += ('a'..'z').to_a
+
+  access_tokens.each do |token|
+    terms.each do |term|
+      events = EventList.new term, token
+      begin
+        events.list.find_all {|e| e.has_location? && e.distance_from(37.7750,-122.4183) < 20}.each do |e|
+          e.save
+        end
+        events.load_next!
+      end while events.has_next?
+    end
   end
 end
