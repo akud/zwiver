c54ce77472dbf5d3a9783cba1dd2baa019dd957b
Alex Kudlick <adkudlick@gmail.com>
2012-08-11 14:02:08 -0700

    add scraper for bay area science calendar

---
+++ scrapers/scrape_ba_sciencecalendar.rb
@@ -0,0 +1,57 @@
+#!/usr/local/bin/ruby -I.
+
+require 'rubygems'
+require 'mechanize'
+require 'time'
+require 'zwiver_event'
+
+module BayAreaScienceCalendar 
+
+  URL = 'http://www.bayareascience.org/calendar/'
+
+# Parse a calendar page and return a list of all 
+# the events on the page
+  def self.get_events page
+    page.links_with(:href => \
+      /http:\/\/www.bayareascience.org\/calendar\/index.php\?eID=\d+/).map do |link|
+      event_page = link.click
+      Zwiver::Event.new :url => event_page.uri.to_s,
+        :title => event_page.search('h1').text,
+        :description => event_page.search('span[itemprop="description"]').text,
+        :date => Time.parse(event_page.search('time').first['content']),
+        :address => event_page.search('span[itemprop="streetAddress"]').text + 
+          ', ' + event_page.search('span[itemprop="addressLocality"]').text + 
+          ' ' + event_page.search('span[iemprop="addressRegion"]').text + 
+          ' ' + event_page.search('span[itemprop="postalCode"]').text,
+        :venue => event_page.search('h2.location').text.strip
+    end
+  end
+
+#get the next page from a calendar page 
+  def self.next_page page
+    page.link_with(:text => 'Browse Forward').click
+  end
+
+#parse pages from the Bay Area Science Calendar,
+#
+#Params:
+#
+# limit - the number of pages to parse (by clicking on the 'next' button)
+  def self.parse_pages limit=5
+    agent = Mechanize.new do |agent|
+      agent.user_agent_alias = 'Linux Firefox'
+    end
+
+    page = agent.get URL
+    limit.times do |i|
+      puts "parsing page #{i + 1}"
+      self.get_events(page).each do |event|
+        event.post
+      end
+      page = self.next_page page
+    end
+  end
+end
+if $0 == __FILE__
+  BayAreaScienceCalendar.parse_pages
+end
