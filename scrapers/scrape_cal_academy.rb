cf9a6520b53daa422a37bf113c03034a13b2aa08
Alex Kudlick <adkudlick@gmail.com>
2012-08-11 16:32:41 -0700

    scraper for cal academy of sciences

---
+++ scrapers/scrape_cal_academy.rb
@@ -0,0 +1,44 @@
+#!/usr/local/bin/ruby -I.
+
+require 'mechanize'
+require 'time'
+require 'zwiver_event'
+
+module CalAcademy
+  
+  ADDRESS = '55 Music Concourse Drive  San Francisco, CA 94118'
+  URL = 'http://www.calacademy.org/events/nightlife/'
+  VENUE = 'California Academy of Sciences'
+
+  def self.scrape
+    agent = Mechanize.new do |agent|
+      agent.user_agent_alias = 'Linux Firefox'
+    end
+    page = agent.get URL 
+    div_list = page.search('div.tabcontentstyle > div')
+    2.times {div_list.pop}
+    div_list.each do |div|
+      title = div.search('div.tab-content-first table:first-child h2').text
+
+#description is a list of paragraphs
+#last 2 are fb and twitter links
+      p_list = div.search('div.tab-content-first table:first-child td:last-child > p')
+      2.times {p_list.pop}
+      description = p_list.text
+
+      day_str = page.search("a[rel='#{div['id']}']").text + ', ' + Time.new.year.to_s
+      date = Time.parse(day_str + ' 8pm PDT')
+      Zwiver::Event.new(:title => title,
+        :date => date,
+        :description => description,
+        :url => URL,
+        :venue => VENUE,
+        :address => ADDRESS).post
+    end
+  end
+end
+
+
+if $0 == __FILE__
+  CalAcademy.scrape
+end
