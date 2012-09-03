dd3d52a8aff14ae2a912ae5c2279f694b243573d
Alex Kudlick <adkudlick@gmail.com>
2012-09-02 20:44:28 -0700

    namespace Mock and delay under QUnit; use QUnit.delay in views test

--- web/test/test_views.js
+++ web/test/test_views.js
@@ -1,140 +1,129 @@
 /********************List View******************/
-module('Views');
-test('ListView', function() {
-  expect(12);
-  equal(ZWVR.listView.get('content'), ZWVR.eventsController.get('content'), 
-    'Controller Binding');
-  
-  //append to the dom
-  ZWVR.listView.appendTo('#qunit-fixture');
-
-  QUnit.stop();
-  setTimeout(function() {
-    equal($('#qunit-fixture li.list-item').length, 
-      ZWVR.eventsController.get('length'), 
-      'appending creates list elements');  
-
-    var childViews = ZWVR.listView.get('childViews');
-    ok(!!childViews.length, 'creates child views');
-
-    //check click behavior
-    ok(childViews.everyProperty('selected',false), 
-      'views are unselected by default');
-    ok(childViews.everyProperty('expanded',false), 
-      'views are unexpanded by default');
-    var clickedItem = $('#qunit-fixture li.list-item:first-child span.show-more')
-      .click();
-    QUnit.stop();
-    setTimeout(function() {
-      ok(childViews[0].get('selected'),'click selects element');
-      ok(!childViews.everyProperty('selected'), 'click selected all elements');
-      equal(childViews[0].get('content'), 
-        ZWVR.eventsController.get('selectedEvent'), 
-        'Click sets selected event on events controller');
-
-      ok(childViews[0].get('expanded'), 
-        'clicking on show more expands list item');  
-      //toggle expanded state
-      clickedItem.click();
-      QUnit.stop();
-      setTimeout(function() {
-        ok(!childViews[0].get('expanded'), 
-          'clicking on show more toggles expanded state');  
-        QUnit.start();
-      }, 20);
-
-      //select another item
-      $('#qunit-fixture li.list-item')[1].click();
-      QUnit.stop();
-      setTimeout(function() {
-        ok(childViews[1].get('selected'),'click selects new element');
-        ok(!childViews[0].get('selected'),'click deselects old element');
-
-        //click the 'show more' link
-        $('#qunit-fixture li.list-item:nth-child(2) span.show-more').click();
-        QUnit.start();
-      }, 20);
-      QUnit.start();
-    }, 20);
-    QUnit.start();
-  }, 20);
-});
-
-/******************Map View*****************/
-test('MapView', function() {
-  expect(3);
-  ZWVR.mapView.appendTo('#qunit-fixture');
-  QUnit.stop();
-  setTimeout(function() {
-    var map = ZWVR.mapView.get('map');
-    ok(map,'creates map object');
-    //the map should have appended a bunch of div children
-    ok($('#qunit-fixture div').length > 10, 'appends map elements');
+$(function() {
+  module('Views');
+  test('ListView', function() {
+    expect(12);
+    equal(ZWVR.listView.get('content'), ZWVR.eventsController.get('content'), 
+      'Controller Binding');
     
-    var ev = ZWVR.Event.create({
-      lat: 30.129,
-      lon: 129.34,
-      title: 'test title',
-      description: 'foo'
-    });
+    //append to the dom
+    ZWVR.listView.appendTo('#qunit-fixture');
+    QUnit.delay(function() {
+      equal($('#qunit-fixture li.list-item').length, 
+        ZWVR.eventsController.get('length'), 
+        'appending creates list elements');  
 
-    ZWVR.eventsController.select(ev);
-    QUnit.stop();
-    setTimeout(function() {
-      equal(Math.round(ZWVR.mapView.get('map').getCenter().lat()), 
-        Math.round(ev.get('position').lat()),
-        'selecting an event pans the map');
-      /*
-      TODO: why aren't info windows opening in the tests?
-      ok(!!$('div.info-window-title').length, 
-        'selecting an event opens the info window')
-      equal($('div.info-window-title').text(), ev.get('title'),
-        'open info window has correct title');
-      */
-      QUnit.start();
-    }, 25);
-  QUnit.start();
-  }, 150);
-});
-test('Sort Buttons', function() {
-  expect(9);
-  //setup mock
-  var sortArgs = [];
-  var mock = new ZWVR.Mock(ZWVR.eventsController, 'sortBy', function(sortType) {
-    sortArgs.push(sortType);
-  }).apply();
+      var childViews = ZWVR.listView.get('childViews');
+      ok(!!childViews.length, 'creates child views');
 
-  //append to DOM
-  ZWVR.sortButtons.appendTo('#qunit-fixture');
-  ZWVR.timeout(function() {
-    ok($('#sort-buttons').length, 'appends correctly');
-    //check clicked state
-    deepEqual(ZWVR.sortButtons.get('childViews').getEach('clicked'), 
-      [true, false], 'default clicked state');
+      //check click behavior
+      ok(childViews.everyProperty('selected',false), 
+        'views are unselected by default');
+      ok(childViews.everyProperty('expanded',false), 
+        'views are unexpanded by default');
+      var clickedItem = $('#qunit-fixture li.list-item:first-child span.show-more')
+        .click();
+      QUnit.delay(function() {
+        ok(childViews[0].get('selected'),'click selects element');
+        ok(!childViews.everyProperty('selected'), 'click selected all elements');
+        equal(childViews[0].get('content'), 
+          ZWVR.eventsController.get('selectedEvent'), 
+          'Click sets selected event on events controller');
 
-    //date sort is already clicked
-    $('#date-sort div').click();
-    ZWVR.timeout(function() {
-      //state should be the same
-      deepEqual(ZWVR.sortButtons.get('childViews').getEach('clicked'), 
-        [true, false], 'clicking selected sorts does not change state');
-      ok(!sortArgs.length, 
-        'clicking selected sort button does not call sort function');
-      
-      //click the distance sort button
-      $('#distance-sort div').click();
-      ZWVR.timeout(function() {
-        deepEqual(ZWVR.sortButtons.get('childViews').getEach('clicked'), 
-          [false, true], 'clicked state changes');
+        ok(childViews[0].get('expanded'), 
+          'clicking on show more expands list item');  
+        //toggle expanded state
+        clickedItem.click();
+        QUnit.delay(function() {
+          ok(!childViews[0].get('expanded'), 
+            'clicking on show more toggles expanded state');  
+        });
 
-        //click the date sort button again
-        $('#date-sort div').click();
-        ZWVR.timeout(function() {
-          deepEqual(ZWVR.sortButtons.get('childViews').getEach('clicked'), 
-            [true, false], 'clicked state changes');
-          deepEqual(sortArgs, [ZWVR.sorts.DISTANCE, ZWVR.sorts.DATE], 'Sort arguments');
-        }, mock.release);
+        //select another item
+        $('#qunit-fixture li.list-item')[1].click();
+        QUnit.delay(function() {
+          ok(childViews[1].get('selected'),'click selects new element');
+          ok(!childViews[0].get('selected'),'click deselects old element');
+
+          //click the 'show more' link
+          $('#qunit-fixture li.list-item:nth-child(2) span.show-more').click();
+        });
       });
-     });
+    });
+  });
+
+  /******************Map View*****************/
+  test('MapView', function() {
+    expect(3);
+    ZWVR.mapView.appendTo('#qunit-fixture');
+    QUnit.delay(300, function() {
+      var map = ZWVR.mapView.get('map');
+      ok(map,'creates map object');
+      //the map should have appended a bunch of div children
+      ok($('#qunit-fixture div').length > 10, 'appends map elements');
+      
+      var ev = ZWVR.Event.create({
+        lat: 30.129,
+        lon: 129.34,
+        title: 'test title',
+        description: 'foo'
+      });
+
+      ZWVR.eventsController.select(ev);
+      QUnit.delay(50, function() {
+        equal(Math.round(ZWVR.mapView.get('map').getCenter().lat()), 
+          Math.round(ev.get('position').lat()),
+          'selecting an event pans the map');
+        /*
+        TODO: why aren't info windows opening in the tests?
+        ok(!!$('div.info-window-title').length, 
+          'selecting an event opens the info window')
+        equal($('div.info-window-title').text(), ev.get('title'),
+          'open info window has correct title');
+        */
+      });
+    });
+  });
+  test('Sort Buttons', function() {
+    expect(9);
+    //setup mock
+    var sortArgs = [];
+    var mock = new QUnit.Mock(ZWVR.eventsController, 'sortBy', function(sortType) {
+      sortArgs.push(sortType);
+    }).apply();
+
+    //append to DOM
+    ZWVR.sortButtons.appendTo('#qunit-fixture');
+    QUnit.delay(function() {
+      ok($('#sort-buttons').length, 'appends correctly');
+      //check clicked state
+      deepEqual(ZWVR.sortButtons.get('childViews').getEach('clicked'), 
+        [true, false], 'default clicked state');
+
+      //date sort is already clicked
+      $('#date-sort div').click();
+      QUnit.delay(function() {
+        //state should be the same
+        deepEqual(ZWVR.sortButtons.get('childViews').getEach('clicked'), 
+          [true, false], 'clicking selected sorts does not change state');
+        ok(!sortArgs.length, 
+          'clicking selected sort button does not call sort function');
+        
+        //click the distance sort button
+        $('#distance-sort div').click();
+        QUnit.delay(function() {
+          deepEqual(ZWVR.sortButtons.get('childViews').getEach('clicked'), 
+            [false, true], 'clicked state changes');
+
+          //click the date sort button again
+          $('#date-sort div').click();
+          QUnit.delay(function() {
+            deepEqual(ZWVR.sortButtons.get('childViews').getEach('clicked'), 
+              [true, false], 'clicked state changes');
+            deepEqual(sortArgs, [ZWVR.sorts.DISTANCE, ZWVR.sorts.DATE], 'Sort arguments');
+          }, mock.release);
+        });
+       });
+    });
   });
 });
--- web/test/utils.js
+++ web/test/utils.js
@@ -1,9 +1,10 @@
 /**
- * Define global test utiltiy functions.
+ * Define test utiltiy functions.
  *
- * @exports Mock
+ * @exports QUnit.Mock class for mocking functions
+ * @exports QUnit.delay wrapper around setTimeout
  */
-(function(ZWVR, $, QUnit) {
+(function($, QUnit) {
   /**
    * Defines a Mock class, to temporarily replace a target function.
    *
@@ -20,7 +21,7 @@
    * @param target string naming the target function. Required 
    * @param replacement the function with which to replace target. Optional.
    */
-  ZWVR.Mock = function(holder, target, replacement) {
+  QUnit.Mock = function(holder, target, replacement) {
     this.original = holder[target];
     this.replacement = replacement;
 
@@ -48,18 +49,18 @@
   };
  
   /**
-   * Set a timeout for a function, ensuring that <code>QUnit.stop()</code> and <code>QUnit.start()</code> are called appropriately.
+   * Set a delay for a function, ensuring that <code>QUnit.stop()</code> and <code>QUnit.start()</code> are called appropriately.
    *
-   * @param timeout number of milliseconds to wait before executing the target function. Optional. Defaults to 20
-   * @param after the function to execute after the timeout. Required.
+   * @param delay number of milliseconds to wait before executing the target function. Optional. Defaults to 20
+   * @param after the function to execute after the delay. Required.
    * @param cleanup any cleanup work to be performed after the target function. Optional.
    */
-  ZWVR.timeout = function(timeout, after, cleanup) {
-    if(typeof(timeout) === 'function') {
-      //timeout argument wasn't supplied, set defaults
+  QUnit.delay = function(delay, after, cleanup) {
+    if(typeof(delay) === 'function') {
+      //delay argument wasn't supplied, set defaults
       after = arguments[0];
       cleanup = arguments[1];
-      timeout = 20;
+      delay = 20;
     }
     QUnit.stop()
     setTimeout(function() {
@@ -71,6 +72,6 @@
           cleanup();
         }
       }
-    }, timeout);
+    }, delay);
   }
-})(ZWVR, jQuery, QUnit);
+})(jQuery, QUnit);
